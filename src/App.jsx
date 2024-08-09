a2987f69c2e3aa5a88ba5d71074cb1ce4e921476
SilverKnight02 <saiparansothi02@gmail.com>
2024-08-09 11:59:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from './Components/NavBar/navbar';
+import Intro from './Components/intro/intro';
+
+function App() {
+
+  return (
+    <>
+       <Navbar />      
+       <Intro />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/intro/intro.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import bg from '../../assets/image.jpeg';
+import btnImg from '../../assets/hire-me.png';
+import './intro.css';
+import { Link } from 'react-scroll';
+
+
+const Intro = () => {
+  return (
+        <section id="intor">
+            <div className="introContent">
+                <span className="hello">Hello !</span>
+                <span className="introText">I'm <span className="introName">Saiparansothi</span><br />Website Designer</span>
+                <p className="introPara">I'm skilled in Website Designing</p>
+                <Link>
+                <button className="btn">
+                    <img src={btnImg} alt="Hire Me" className="btnImg" />Hire Me
+                </button>
+                </Link>
+            </div>
+            <img src={bg} alt="" className="bg" />
+        </section>
+  )
+}
+
+export default Intro;
